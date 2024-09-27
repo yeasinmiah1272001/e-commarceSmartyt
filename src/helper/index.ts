16e01e00f17b8c296e0b0b30683dd6151fdd8722
yeasinmiah1272001 <yeasinmiah1272001@gmail.com>
2024-09-28 03:05:51 +0600

    local

--- src/helper/index.ts
+++ src/helper/index.ts
@@ -1,18 +1,18 @@
 import { productData } from "@/cconstant/data";
 
 export const getProducts = async () => {
-    const res = await fetch("https://fakestoreapiserver.reactbd.com/smart");
-    // const res = await fetch("http://localhost:3000/api/products");
-    return res.json();
-  };
+  const res = await fetch("https://fakestoreapiserver.reactbd.com/smart");
+  return res.json();
+};
 
 export const getTraningProducts = async () => {
-    const res = await fetch("https://fakestoreapiserver.reactbd.com/smarttrending");
-    return res.json();
-  };
+  const res = await fetch(
+    "https://fakestoreapiserver.reactbd.com/smarttrending"
+  );
+  return res.json();
+};
 
-
-  export const singleProduct = (_id:number) =>{
-    const item = productData.find((product) => product._id === _id)
-    return item
-  }
+export const singleProduct = (_id: number) => {
+  const item = productData.find((product) => product._id === _id);
+  return item;
+};
