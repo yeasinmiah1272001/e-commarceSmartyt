a11d9b085e111de0f9aad69defb21b343a0ff9d5
yeasinmiah1272001 <yeasinmiah1272001@gmail.com>
2024-09-28 03:03:48 +0600

    dfhd

--- src/helper/AuthIndex.ts
+++ src/helper/AuthIndex.ts
@@ -1,28 +1,8 @@
-// import NextAuth, { NextAuthOptions } from "next-auth"
-// import GithubProvider from "next-auth/providers/github"
-// import GoogleProvider from "next-auth/providers/google"
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
-// export const authOptions:NextAuthOptions = {
-//   // Configure one or more authentication providers
-//   providers: [
-//     GithubProvider({
-//       clientId: process.env.GITHUB_ID!,
-//       clientSecret: process.env.GITHUB_SECRET!,
-//     }),
-//     GoogleProvider({
-//         clientId: process.env.GOOGLE_CLIENT_ID!,
-//         clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-//       })
- 
-//   ],
-// }
-
-
-import NextAuth, { NextAuthOptions } from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
-
-export const authOptions:NextAuthOptions = {
+export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
@@ -30,9 +10,8 @@ export const authOptions:NextAuthOptions = {
       clientSecret: process.env.GITHUB_SECRET!,
     }),
     GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID!,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-      })
- 
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
   ],
-}
+};
